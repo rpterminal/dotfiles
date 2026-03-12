ac76db222eb184d76cd3dbf394149f3349c78a35
rpterminal <luizrenanpontes@gmail.com>
2026-03-12 17:48:12 -0300

    cht.sh++

--- bin/cht.sh
+++ bin/cht.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-languages=$(echo "cpp c lua python bash javascript html css" | tr ' ' '\n')
-core_utils=$(echo "find xargs sed awk grep nmap tcpdump curl tar tmux" | tr ' ' '\n')
+languages="cpp c lua python bash javascript html css"
+core_utils="find xargs sed awk grep nmap tcpdump curl tar tmux"
 
-selected=$(echo -e "$languages\n$core_utils" | fzf --prompt="Cheat.sh > " --layout=reverse --border)
+selected=$(echo -e "${languages// /\\n}\n${core_utils// /\\n}" | fzf --prompt="Cheat.sh > " --layout=reverse --border)
 
-if [[ -z $selected ]]; then
-    exit 0
-fi
+[[ -z $selected ]] && exit 0
 
-read -p "$selected (Query): " query
+read -p "Query ($selected): " query
+query=$(echo "$query" | tr ' ' '+')
 
-query=$(echo $query | tr ' ' '+')
-
-if echo "$languages" | grep -qs $selected; then
-    curl -s cht.sh/$selected/$query | bat -R
+if echo "$languages" | grep -qw "$selected"; then
+    lang="$selected"
+    [[ -n "$query" ]] && url="cht.sh/$selected/$query?T" || url="cht.sh/$selected?T"
 else
-    curl -s cht.sh/$selected~$query | bat -R
+    lang="bash"
+    [[ -n "$query" ]] && url="cht.sh/$selected~$query?T" || url="cht.sh/$selected?T"
 fi
+
+curl -s "$url" | bat --language="$lang" --style=plain --paging=always
